e118f217fbc8a6cfbfcbec81429520db1fb1265e
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-24 20:13:04 +0200

    Project now calls API for specific word.

--- src/components/pages/DetailsPage.tsx
+++ src/components/pages/DetailsPage.tsx
@@ -1,20 +1,42 @@
-import {Link} from "react-router-dom";
+import { useEffect, useState } from 'react';
 import {useParams} from "react-router-dom";
+import getWordDetails, {WordData} from "../../services/WordSearchSevice.ts";
 
 const DetailsPage = () => {
-    const { word } = useParams()
-    return (
-        <>
-            <nav>
-                <Link to="/">Back to search</Link>
-            </nav>
-            <div>
-                <h1>Word Details</h1>
-                <p>Selected word: {word}</p>
-                {/* TODO display the details */}
-            </div>
-        </>
-    );
+  const { word } = useParams();
+  const [wordDetails, setWordDetails] = useState<WordData | null>(null);
+
+  useEffect(() => {
+    getWordDetails(word).then(details => setWordDetails(details));
+  }, [word]);
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => window.history.back()}>
+          Back to search
+        </button>
+      </nav>
+      <div>
+        <h1>{word}</h1>
+        {wordDetails && (
+          <>
+            <h3>Phonetic: {wordDetails.phonetic}</h3>
+            {wordDetails.meanings.map((meaning, index) => (
+              <div key={index}>
+                <h4>Part Of Speech: {meaning.partOfSpeech}</h4>
+                <ul>
+                  {meaning.definitions.map((definition, defIndex) => (
+                    <li key={defIndex}>{definition.definition}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default DetailsPage;
--- src/services/WordSearchSevice.ts
+++ src/services/WordSearchSevice.ts
@@ -1,3 +1,48 @@
+import axios from 'axios';
+
+interface Definition {
+    definition: string;
+}
+
+interface Meaning {
+    partOfSpeech: string;
+    definitions: Definition[];
+}
+
+export interface WordData {
+    word: string;
+    phonetic: string;
+    meanings: Meaning[];
+}
+
+const parseWordData = (data: any): WordData => ({
+    word: data.word,
+    phonetic: data.phonetic,
+    meanings: data.meanings.map((meaning: any) => ({
+        partOfSpeech: meaning.partOfSpeech,
+        definitions: meaning.definitions.map((def: any) => ({
+            definition: def.definition
+        }))
+    }))
+});
+
+const getWordDetails = async (word: string | undefined): Promise<WordData | null> => {
+    if (word === undefined)
+        return null
+    try {
+        const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = response.data;
+
+        return data.length > 0 ? parseWordData(data[0]) : null;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+export default getWordDetails;
+
+
 export async function wordSearch(query: string) {
     if (!query)
         return []
