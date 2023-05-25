69d1f5d3c6b70cd838040223506331521249159e
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-25 14:44:14 +0200

    Fixed the bug with searching multiwords, like 'hot dog'.

--- src/services/WordSearchSevice.ts
+++ src/services/WordSearchSevice.ts
@@ -1,36 +1,23 @@
 import axios, {AxiosError} from 'axios';
 import {ErrorObject, WordData} from "../models/dataModels.ts";
 
-const parseWordData = (data: any): WordData => ({
-    word: data.word,
-    phonetic: data.phonetic,
-    meanings: data.meanings.map((meaning: any) => ({
-        partOfSpeech: meaning.partOfSpeech,
-        definitions: meaning.definitions.map((def: any) => ({
-            definition: def.definition
-        }))
-    }))
-});
-
 const getWordDetails = async (word: string | undefined): Promise<WordData | ErrorObject | null> => {
     if (word === undefined) {
         return null;
     }
 
     try {
-        const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const response = await axios.get(getParsedUrl(word));
         const data = response.data;
 
         return data.length > 0 ? parseWordData(data[0]) : null;
+
     } catch (error) {
         const axiosError = error as AxiosError;
 
         if (axiosError.response && axiosError.response.status === 404) {
-            return {
-                title: 'No Definitions Found',
-                message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
-                resolution: 'You can try the search again at a later time or head to the web instead.',
-            };
+            return getErrorObject();
+
         } else {
             console.error('Error: ', axiosError.message);
         }
@@ -44,4 +31,29 @@ export async function wordSearch(query: string) {
     if (!query)
         return []
     return ["Alpha", "Albino", "Albatross", query]
+}
+
+function getParsedUrl(word: string) {
+    word = word.trim();
+    word = word.split(' ').join('%20');
+    return `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+}
+
+const parseWordData = (data: any): WordData => ({
+    word: data.word,
+    phonetic: data.phonetic,
+    meanings: data.meanings.map((meaning: any) => ({
+        partOfSpeech: meaning.partOfSpeech,
+        definitions: meaning.definitions.map((def: any) => ({
+            definition: def.definition
+        }))
+    }))
+});
+
+function getErrorObject() {
+    return {
+        title: 'No Definitions Found',
+        message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
+        resolution: 'You can try the search again at a later time or head to the web instead.',
+    };
 }
